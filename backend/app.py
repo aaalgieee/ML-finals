ccea8143637bcd606a451d8308a0abf20b9c7549
Al Gabriel <jaworski.orig@gmail.com>
2024-11-21 12:51:44 +0800

    fix: build port

--- backend/app.py
+++ backend/app.py
@@ -221,5 +221,5 @@ def health_check():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
